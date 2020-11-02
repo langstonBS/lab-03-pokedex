2aef04f862055ffbee0feb29d51950c21f9ffc30
Langston Beckwith Stanley <langstonbeckwithstanley@langstons-mac.lan>
2020-11-02 13:35:18 -0800

    The application looks amzing just some notes on sort and direction

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ import Navbar from "./Navbar.js";
 
 export default class App extends Component {
   state = {
-    selectedCategory: "",
-    selectedSort: "",
+    selectedCategory: "",  //State for sort : 'somthing you want to sort by'
+    selectedSort: "",  //State for direction : 'asc'
     inputVal: "",
     pokedex: [],
     loading: true,
@@ -20,11 +20,17 @@ export default class App extends Component {
   };
 
   async handlePageChange(pageNumber) {
+    //pagination logic hear would be good to then you can just have one fech call
+    //It will allow for pagination when serching for pokemon 
     await this.setState({
       activePage: pageNumber,
     });
     this.fetchPokemonAPI();
   }
+
+  //have a handle change for sort
+
+   //have a handle change for directions
 
   handlePokemonClick = async (pokemon) => {
     this.props.history.push(`/pokemon/${pokemon.pokemon}`);
@@ -63,6 +69,7 @@ export default class App extends Component {
     if (this.state.inputVal.length > 0) {
       const res = await request.get(
         `https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=${this.state.inputVal}`
+        // `https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=${this.state.inputVal}&sort=${this.state.sort}&direction=${this.state.order}&page=${this.state.activePage}&perPage=${this.state.perPage}``
       );
       return this.setState({
         pokedex: res.body.results,
@@ -70,6 +77,7 @@ export default class App extends Component {
       });
     } else {
       const res = await request.get(
+        //I would put the pagination in the same url as everting else it would allow you to paginate throug the sorting
         `https://alchemy-pokedex.herokuapp.com/api/pokedex?page=${this.state.activePage}&perPage=${this.state.perPage}`
       );
       return this.setState({
